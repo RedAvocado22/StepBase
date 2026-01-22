d3b7c94ebd6c90bfddd12853b4582655c7723109
RedAvocado <minhcuong2922004@gmail.com>
2026-01-22 09:02:48 +0700

    initial code base

---
+++ src/main/java/com/stepbase/stepbase_api/StepBaseApplication.java
@@ -0,0 +1,13 @@
+package com.stepbase.stepbase_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stepbase/stepbase_api/StepBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stepbase.stepbase_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
